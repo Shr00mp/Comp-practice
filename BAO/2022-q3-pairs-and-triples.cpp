d34e3f570004ca004ff6e8606560ec1e0d5250eb
Sharon <147839877+Shr00mp@users.noreply.github.com>
2025-11-29 20:16:31 +0000

    All test cases passedgit add -A

---
+++ BAO/2022-q3-pairs-and-triples.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <cmath>
+#include <algorithm>
+
+int part_c(){
+    long long n; cin >> n;
+    long long total = 0;
+    long long top_a = sqrt(n);
+    long long remainder;
+    long long top_b;
+    for (long long a=0; a<=top_a; a++) {
+        remainder = n - pow(a, 2);
+        top_b = sqrt(remainder);
+        if (pow(top_b, 2) + pow(a, 2) == n) {
+            total += top_b;
+        } else {
+            total += top_b + 1;
+        }
+    }
+    return total;
+}
+
+int main() {
+    long long total = part_c();
+    cout << total;
+}
