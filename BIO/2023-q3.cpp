2a92dc9aa2fd5e96ce296c0400fa01cbb5562b9c
Sharon <147839877+Shr00mp@users.noreply.github.com>
2026-01-04 17:33:29 +0000

    TLE with BFS

---
+++ BIO/2023-q3.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool already_visited(vector<vector<string>> &visited, vector<string> &curr) {
+    for (vector<string> s: visited) 
+        if (s == curr) return true;
+    return false;
+}
+
+void part_a() {
+    vector<string> original;
+    string s;
+    for (int i=0; i<4; i++) {
+        cin >> s; original.push_back(s);
+    }
+
+    vector<string> goal;
+    for (int i=0; i<4; i++) {
+        cin >> s; goal.push_back(s);
+    }
+    
+    
+    // conduct a bfs
+    queue<tuple<vector<string>, int>> q;
+    q.push({original, 0});
+    vector<vector<string>> visited;
+    vector<string> current; vector<string> neighbour;
+    int level;
+    if (original == goal) {
+        level=-1;
+        goto end;
+    }
+    while (!q.empty()) {    
+        current = get<0>(q.front());
+        level = get<1>(q.front());
+        q.pop();
+        visited.push_back(current);
+        // generate next combinations
+        // by moving top of i to top of j
+        for (int moving=0; moving<4; moving++) {
+            if (current[moving] == "0") continue;
+            for (int to=0; to<4; to++) {
+                if (to == moving) continue;
+                neighbour = current;
+                if (neighbour[to] == "0") neighbour[to] = "";
+                neighbour[to] += current[moving].back();
+                neighbour[moving].pop_back();
+                if (neighbour[moving] == "") neighbour[moving] = "0";
+                if (neighbour == goal) {goto end;}
+                if (!already_visited(visited, neighbour)) q.push({neighbour, level+1}); 
+            }
+        }
+    }
+    end:;
+    cout << level+1;
+
+    return; 
+}
+
+int main() {
+    part_a();
+}
